7f79ac47b800d66ea53fc088fe07792416fd1fe3
dave motari <davemotari22@gmail.com>
2022-08-25 06:05:25 +0300

    Correction of Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,8 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-/* if (!parent) return NULL;*/
+if (!parent)
+return (NULL);
 parent->left = binary_tree_node(parent, value);
 return (parent->left);
 }
