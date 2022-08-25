38b474ca5ef826ed459e13c4d165593f1585653c
dave motari <davemotari22@gmail.com>
2022-08-25 08:34:52 +0300

    Task 5 correction

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,4 +8,15 @@
  * Return: 1 if node is a leaf, otherwise 0
            If node is NULL, return 0
  */
-int binary_tree_is_root(const binary_tree_t *node){}
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (node == NULL)
+{
+return (0);
+}
+if (node->parent == NULL)
+{
+return (1);
+}
+return (0);
+}
