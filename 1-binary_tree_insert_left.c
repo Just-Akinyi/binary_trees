5776dbf0c4830b2fdead77f0705db335a2fc156f
dave motari <davemotari22@gmail.com>
2022-08-25 05:52:44 +0300

    Task 1 correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,11 +5,13 @@
  *
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
- * 
- * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ *
+ * Return: a pointer to the created node,
+ * or NULL on failure or if parent is NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value){
-    // if (!parent) return NULL;
-    parent->left = binary_tree_node(parent, value);
-    return parent->left;
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+/* if (!parent) return NULL;*/
+parent->left = binary_tree_node(parent, value);
+return (parent->left);
 }
